07c5f2fd72212657f4e4ee6d085c73e77a90ca29
salunchana Sonkhuntod <Salunchana2543@gmail.com>
2020-07-04 14:32:42 +0700

    4Complete

---
+++ 62120501010-PEXAM-MDT112/4Complete/4Complete.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+
+Servo myservo;
+void setup() {
+    
+    Serial.begin(9600);
+    myservo.write(0);
+    myservo.attach(9);
+    delay(1000);
+    myservo.write(180);
+    delay(900);
+    Serial.println("Servo:0 degrees");
+}
+void loop() {
+    
+    
+}
